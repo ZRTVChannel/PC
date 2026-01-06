028b69f75ce40b10e2075680b71b2993b4cc6172
ZRTVCHANNEL <163850422+ZRTVChannel@users.noreply.github.com>
2026-01-06 10:21:57 +0800

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -63,7 +63,7 @@ if [ "$EXISTING_INSTALL" = false ]; then
 
     # --- Download Gambar Profil ---
     echo "   📥 Mengunduh Avatar..."
-    wget -q -O "$OEM_DIR/avatar.jpg" "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+    wget -q -O "$OEM_DIR/avatar.jpg" "https://i.pinimg.com/736x/b8/c6/b3/b8c6b3bfba03883bc4fd243d0e80a8a3.jpg"
     chmod 777 "$OEM_DIR/avatar.jpg"
 
     # --- SCRIPT INJECTOR (CMD Exit -> Popup) ---
